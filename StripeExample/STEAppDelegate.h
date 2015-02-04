98bf36bbd509a652904dc7c9b0d629db432cf9f0
Benedikt Terhechte <terhechte@gmail.com>
2015-02-04 09:58:57 +0100

    Initial Commit. Working App.

--- StripeExample/STEAppDelegate.h
+++ StripeExample/STEAppDelegate.h
@@ -7,9 +7,22 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
 
 @interface STEAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSWindow *sheetWindow;
+@property (assign) IBOutlet WebView *webView;
+
+@property (assign) IBOutlet NSProgressIndicator *progressIndicator;
+
+@property (assign) IBOutlet NSTextField *customURLField;
+
+- (IBAction)showBug:(id)sender;
+- (IBAction)cancelBug:(id)sender;
+- (IBAction)showNonBug1:(id)sender;
+- (IBAction)showNonBug2:(id)sender;
+- (IBAction)goCustomURL:(id)sender;
 
 @end
--- StripeExample/STEAppDelegate.m
+++ StripeExample/STEAppDelegate.m
@@ -10,9 +10,72 @@
 
 @implementation STEAppDelegate
 
+#pragma mark NSAppDelegate
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [self.webView setFrameLoadDelegate:self];
+}
+
+#pragma mark Custom
+
+- (void) openWithURL:(NSString*)urlString {
+    [NSApp beginSheet:self.sheetWindow
+       modalForWindow:self.window
+        modalDelegate:nil
+       didEndSelector:NULL
+          contextInfo:nil];
+    [self.webView setMainFrameURL:urlString];
+}
+
+#pragma mark IB Actions
+
+- (IBAction)showBug:(id)sender {
+    [self openWithURL:@"https://stripe.com/docs/checkout"];
+}
+
+- (IBAction)cancelBug:(id)sender {
+    [NSApp endSheet:self.window];
+    [NSApp endSheet:self.sheetWindow];
+    [self.sheetWindow orderOut:nil];
+    [self.webView setMainFrameURL:@"about:blank"];
+}
+
+- (IBAction)showNonBug1:(id)sender {
+    [self openWithURL:@"http://lab.hakim.se/avgrund/"];
+}
+
+- (IBAction)showNonBug2:(id)sender {
+    [self openWithURL:@"http://mikekus.com/portfolio"];
+}
+
+- (IBAction)goCustomURL:(id)sender {
+    NSString *urlString = [self.customURLField stringValue];
+    if (!urlString || urlString.length <= 7) {
+        NSBeep();
+        return;
+    }
+    NSURL *testURL = [NSURL URLWithString:urlString];
+    if (!testURL) {
+        NSBeep();
+        return;
+    }
+    
+    [self openWithURL:urlString];
+}
+
+#pragma mark Web Frame Load Delegate
+
+- (void)webView:(WebView *)sender
+didStartProvisionalLoadForFrame:(WebFrame *)frame {
+    [self.progressIndicator setHidden:NO];
+    [self.progressIndicator startAnimation:nil];
+}
+
+- (void)webView:(WebView *)sender
+didFinishLoadForFrame:(WebFrame *)frame {
+    [self.progressIndicator setHidden:YES];
+    [self.progressIndicator stopAnimation:nil];
 }
 
 @end
